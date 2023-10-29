7836b0b10f79523895220c18e9a37c34c3e85f31
DakshGopani36 <86640269+dakshgopani@users.noreply.github.com>
2023-10-29 18:58:01 +0530

    Add files via upload

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div>This is an about component
+        <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex consectetur minima explicabo veniam vero autem aliquid dolores, non vitae iure tempora aliquam? Expedita odit laudantium non culpa veritatis ex quisquam.  </p>
+    </div>
+    
+  )
+}
---
+++ src/MyComponents/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export const Footer = () => {
+  // let footerStyle = {
+  //   position:"relative",
+  //   top:"70vh",
+  //   width:"100%"
+  // }
+  // style={footerStyle}
+  return (
+    <footer className="bg-dark py-3 text-light" > 
+   
+      <p className="text-center">
+        Copyright &copy; MyTodoList.com
+      </p>
+    
+    </footer>
+  )
+}
